f1e1c373e938c81c780db2d4a814d8c8ed570c32
PacLeonardo97 <56130116+PacLeonardo97@users.noreply.github.com>
2025-07-01 23:34:05 -0300

    Feature/fix buildplanner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { locales } from '@/i18n/config';
 import Providers from '@/providers';
 
 import './globals.css';
+import Script from 'next/script';
 
 type IProps = {
   children: ReactNode;
@@ -29,6 +30,11 @@ export default function RootLayout({ children }: Readonly<IProps>) {
   return (
     <html lang="pt">
       <body className={`${raleway.variable}`}>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2641808103356301"
+          crossOrigin="anonymous"
+        ></Script>
         <NextIntlClientProvider messages={messages}>
           <Providers>
             <Layout />
--- src/enum/equips.enum.ts
+++ src/enum/equips.enum.ts
@@ -55,6 +55,32 @@ export enum EEquipSet {
   true_dragon = 'true_dragon',
   dragon = 'dragon',
   void = 'void',
+  anklet_zoro = 'anklet_zoro',
+  anklet_real = 'anklet_real',
+  anklet_magica = 'anklet_magica',
+  black_dragon_blue = 'black_dragon_blue',
+  black_dragon_red = 'black_dragon_red',
+  death = 'death',
+  destiny = 'destiny',
+  fire = 'fire',
+  grand_chase = 'grand_chase',
+  halloween = 'halloween',
+  heart = 'heart',
+  ice = 'ice',
+  lifeblood = 'lifeblood',
+  light = 'light',
+  lightning = 'lightning',
+  rose = 'rose',
+  shadow = 'shadow',
+  unicorn = 'unicorn',
+  wind = 'wind',
+}
+
+export enum EEquipWeapon {
+  pha_pho = 'pha_pho',
+  true_dragon = 'true_dragon',
+  dragon = 'dragon',
+  void = 'void',
 }
 
 export enum ESTEquip {
@@ -62,4 +88,12 @@ export enum ESTEquip {
   class_2 = 'class_2',
   class_3 = 'class_3',
   class_4 = 'class_4',
+}
+
+export enum ERarityColor {
+  common = 'linear-gradient(to bottom, #7d7d7d, #9a9a9a)',
+  epic = 'linear-gradient(to bottom, #c58e68, #cca46f)',
+  rare = 'yellow',
+  relic = 'linear-gradient(to bottom, #895db9, #c073cc)',
+  ancient = 'linear-gradient(to bottom, #d36471, #d97169)',
 }
